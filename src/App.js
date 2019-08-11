062027fce2052de47b49791a527fad628841eb31
Akhil Seshan <akhilses@gmail.com>
2019-08-12 00:26:27 +0530

    changes :)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
+import cheerio from 'cheerio';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      datas: [],
+    }
+  }
+
+  componentDidMount() {
+    axios.get(`http://localhost:5000/api/getdetails`)
+      .then(res => {
+        this.setState({ datas: res.data });
+        console.log(this.state.datas);
+        for (var i = 2; i < this.state.datas[1].totalcount; i++) {
+          console.log(this.state.datas[i].subject)
+          const $ = cheerio.load(this.state.datas[i].msgbody);
+          $('li').each(function (i, e) {
+            console.log($(this).text());
+          })
+        }
+      })
+      .catch(err => console.log(err))
+  }
+
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
 }
 
 export default App;
