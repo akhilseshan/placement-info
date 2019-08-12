8b8ed5db7db849195982edf570c6e3695a7d7260
Akhil Seshan <akhilses@gmail.com>
2019-08-12 15:49:20 +0530

    changes :)

--- src/App.js
+++ src/App.js
@@ -9,27 +9,38 @@ class App extends Component {
     this.state = {
       datas: [],
     }
+    this.handleLoad = this.handleLoad.bind(this)
   }
 
-  componentDidMount() {
+  getStuff() {
     axios.get(`http://localhost:5000/api/getdetails`)
       .then(res => {
         this.setState({ datas: res.data });
-        console.log(this.state.datas);
+        //console.log(this.state.datas);
         for (var i = 2; i < this.state.datas[1].totalcount; i++) {
-          console.log(this.state.datas[i].subject)
+          // console.log(this.state.datas[i].subject)
           const $ = cheerio.load(this.state.datas[i].msgbody);
           $('li').each(function (i, e) {
-            console.log($(this).text());
+            //console.log($(this).text());
           })
         }
       })
       .catch(err => console.log(err))
   }
 
+  handleLoad() {
+    console.log(this.state);
+  }
+
+  componentDidMount() {
+    this.getStuff();
+    console.log(this.state);
+  }
+
   render() {
     return (
       <div className="App">
+        <button onClick={this.handleLoad}>Click Me</button>
       </div>
     );
   }
